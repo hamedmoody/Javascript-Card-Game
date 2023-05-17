a34a5ef43b16016df044d37fe0f0bdebad00a44d
Hamed Moodi <hamedmoodi2011@gmail.com>
2023-05-17 16:47:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 $(document).ready(function(){
 
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    
     function send_message( text ){
         $.post('https://notificator.ir/api/v1/send', {
             to      : 'B1zLOsSjM7EV9YlQ5X8UHkBFsUZTsVdXtcnd9bMc',
             text    : text,
         });
     }
+    
+    var fruits = [
+        'apple',
+        'avokado',
+        'banana',
+        'blackberry',
+        'cherry',
+        'grape',
+        'mango',
+        'melon',
+        'nectarine',
+        'orange',
+        'pear',
+        'pomegranate',
+        'strawberry',
+        'tomato',
+        'watermelon',
+        'apple',
+        'avokado',
+        'banana',
+        'blackberry',
+        'cherry',
+        'grape',
+        'mango',
+        'melon',
+        'nectarine',
+        'orange',
+        'pear',
+        'pomegranate',
+        'strawberry',
+        'tomato',
+        'watermelon',
+    ];
 
 
 
